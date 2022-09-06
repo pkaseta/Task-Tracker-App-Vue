da8ec75457d50235e5c62b51dcad9dc0f4e741d2
Paul Kaseta Jr <kasetapaul@gmail.com>
2022-09-06 15:54:46 -0400

    Changed X button to white color to match the layout better

--- src/components/SingleTask.vue
+++ src/components/SingleTask.vue
@@ -34,7 +34,7 @@ export default {
 <style scoped>
   .fas
   {
-    color: red;
+    color: white;
   }
   .tasks-container {
     padding: 5px;
@@ -59,5 +59,8 @@ export default {
     align-items: center;
     border: 1px solid white;
     border-radius: 5px;
+  }
+  .tasks-content :hover {
+
   }
 </style>
