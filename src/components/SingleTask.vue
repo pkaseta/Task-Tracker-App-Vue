69528eb460ef469a99600bccdf6440ef03065430
Paul Kaseta Jr <kasetapaul@gmail.com>
2022-09-06 14:31:30 -0400

    Updated styling to individual tasks

--- src/components/SingleTask.vue
+++ src/components/SingleTask.vue
@@ -1,9 +1,15 @@
 <template>
-  <h3>{{task.text}}
-    <i class="fas fa-times"></i>
-  </h3>
-  <p>{{task.day}}</p>
-
+  <div class="tasks-container">
+    <h3>{{task.text}}
+      <i class="fas fa-times"></i>
+    </h3>
+    <p>{{task.day}}</p>
+    <p>
+      <strong>
+        Reminder: {{ task.reminder ? 'Yes' : 'No' }}
+      </strong>
+    </p>
+  </div>
 </template>
 
 <script>
@@ -21,5 +27,13 @@ export default {
   {
     color: red;
     margin-left: 10px;
+  }
+  .tasks-container {
+    padding: 5px;
+    border-radius: 5px;
+    border: 1px solid black;
+    display: flex;
+    flex-direction: column;
+    align-items: flex-start;
   }
 </style>
